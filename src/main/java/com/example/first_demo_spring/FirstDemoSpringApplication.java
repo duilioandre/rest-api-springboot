5e98b03c045c4ac4f439d75c74cad593eb33459a
andre <duilioandrecastro@gmail.com>
2022-06-14 21:18:45 +0200

    init commit

---
+++ src/main/java/com/example/first_demo_spring/FirstDemoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_demo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstDemoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/first_demo_spring/FirstDemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first_demo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
