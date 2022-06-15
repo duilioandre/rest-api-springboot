9fac273201c1fe5d0ad072d39574059e5a903776
andre <duilioandrecastro@gmail.com>
2022-06-15 22:57:59 +0200

    feat: db connected
    - Spring boot data jpa dependency added in pom
    - Implmented jpa annotations in student class
    - Database connection properties added in application.properties file

--- src/main/java/com/example/first_demo_spring/student/Student.java
+++ src/main/java/com/example/first_demo_spring/student/Student.java
@@ -1,9 +1,19 @@
 package com.example.first_demo_spring.student;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "studente")
 public class Student {
+    @Id
+    @SequenceGenerator(name = "student_squence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private int id_studente;
+    @Column(name = "nome", length = 45, nullable = false)
     private String name;
+    @Column(name = "cognome", length = 45, nullable = false)
     private String cognome;
+    @Column(name = "email", length = 45, nullable = false)
     private String email;
 
     public Student(int id_studente, String name, String cognome, String email) {
@@ -11,6 +21,10 @@ public class Student {
         this.name = name;
         this.cognome = cognome;
         this.email = email;
+    }
+
+    public Student() {
+
     }
 
     public int getId_studente() {
