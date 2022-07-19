ed2338901510fee4404d80a8a4513a7193e03555
andre <duilioandrecastro@gmail.com>
2022-07-19 12:30:03 +0200

    fix: removed unused import

--- src/main/java/com/example/first_demo_spring/student/StudentController.java
+++ src/main/java/com/example/first_demo_spring/student/StudentController.java
@@ -3,7 +3,6 @@ package com.example.first_demo_spring.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
